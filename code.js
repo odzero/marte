5433a15cc61c13c4e32426440f982bce2361dd5a
Oscar <zero.nextar@gmail.com>
2016-02-03 22:21:00 -0600

    add event listener listo

--- code.js
+++ code.js
@@ -1,3 +1,18 @@
+var cajaDeTexto = document.getElementById("formulario");
+var boton = document.getElementById("boton");
+
+boton.addEventListener("click", formula);
+
+function formula()
+
+	{
+		alert("Funciona");
+	}
+
+
+
+
+/*
 var peso;
 var pesoMarte;
 
@@ -8,8 +23,7 @@ peso = parseInt(peso);
 pesoMarte = (peso/9.81)*3.711;
 
 alert("Tu peso en marte es " + pesoMarte);
-
-
+*/
 
 
 
